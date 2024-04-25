5eed98b9131d69f64efb90864a68791a706b8f58
Gabor Szabo <gabor@szabgab.com>
2024-04-25 17:29:26 +0300

    test echo

--- src/tests.rs
+++ src/tests.rs
@@ -7,6 +7,17 @@ fn hello_world() {
     let client = Client::tracked(super::rocket()).unwrap();
     let response = client.get("/").dispatch();
 
+    assert_eq!(response.headers().get_one("Content-Type").unwrap(), "text/html; charset=utf-8");
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(response.into_string(), Some("Hello, world!".into()));
+
+}
+
+#[test]
+fn test_echo() {
+    let client = Client::tracked(super::rocket()).unwrap();
+    let response = client.get("/echo?text=Foo+Bar").dispatch();
+
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.into_string(), Some("Echo: Foo Bar".into()));
 }
