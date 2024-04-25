d52cf8efced3dfcedcdfe5f263101a922d6f810e
Gabor Szabo <gabor@szabgab.com>
2024-04-25 15:39:19 +0300

    move test to separate file

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,4 @@ fn rocket() -> _ {
 
 
 #[cfg(test)]
-mod test {
-    use rocket::http::Status;
-    use rocket::local::blocking::Client;
-
-    #[test]
-    fn hello_world() {
-        let client = Client::tracked(super::rocket()).unwrap();
-        let response = client.get("/").dispatch();
-
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string(), Some("Hello, world!".into()));
-    }
-}
+mod tests;
