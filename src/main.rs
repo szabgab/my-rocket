4b149a5a66c3b37c0dcd5b34fbc5825624d8f82f
Gabor Szabo <gabor@szabgab.com>
2024-04-25 17:07:01 +0300

    logging and echo

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,20 @@ use rocket::response::content;
 
 #[get("/")]
 fn index() -> content::RawHtml<&'static str> {
+    rocket::info!("some info message");
     content::RawHtml("Hello, world!")
+}
+
+#[get("/echo?<text>")]
+fn echo(text: String) -> content::RawHtml<String> {
+    rocket::info!("Received: {text:?}");
+    content::RawHtml(format!("Echo: {text}"))
 }
 
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, echo])
 }
 
 
